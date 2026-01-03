be08a5a53fbc58e6b96128138a51a1bf75bbf3e8
karimovsherali23 <karimovsherali786@gmail.com>
2026-01-04 02:18:04 +0500

    Update API token and improve error handling

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from aiogram.types import BufferedInputFile, ReplyKeyboardMarkup, KeyboardButton
 from threading import Thread
 
 # --- SOZLAMALAR ---
-API_TOKEN = '8110490890:AAG8zb1QoXeXJilV5Ot0_iyUN502Tt8e0Xk'
+API_TOKEN = '8110490890:AAEbRJudOQHI5dkZ8d5nelYi0wf5aey4RVQ'
 ADMIN_ID = 7693087447 
 SHEET_NAME = "QR_Biznes" 
 BASE_URL = "https://bot2-l6hj.onrender.com" 
@@ -157,4 +157,5 @@ async def run_bot():
 if __name__ == "__main__":
     Thread(target=run_flask).start()
     asyncio.run(run_bot())
+
 
